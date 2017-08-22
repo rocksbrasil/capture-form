409ac6e13e33caa64381b85fdfc5cbcb3283a370
Rocks Brasil Team <contato@rocksbrasil.com.br>
2017-08-22 14:26:37 -0300

    versão 1.0

--- templates/compiled/37eef6348cbde79e27f29bda3b070948d32275ba_0.file.content.html.php
+++ templates/compiled/37eef6348cbde79e27f29bda3b070948d32275ba_0.file.content.html.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2017-08-22 04:55:56
+/* Smarty version 3.1.29, created on 2017-08-22 13:56:19
   from "C:\wamp64\www\capture-form\templates\content.html" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_599be38cdec8c4_60688149',
+  'unifunc' => 'content_599c6233537e09_38453498',
   'file_dependency' => 
   array (
     '37eef6348cbde79e27f29bda3b070948d32275ba' => 
     array (
       0 => 'C:\\wamp64\\www\\capture-form\\templates\\content.html',
-      1 => 1503388555,
+      1 => 1503420926,
       2 => 'file',
     ),
   ),
@@ -19,10 +19,36 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_599be38cdec8c4_60688149 ($_smarty_tpl) {
+function content_599c6233537e09_38453498 ($_smarty_tpl) {
 ?>
-<div id="content" class="padding whitebox">
-	<h1>Em breve</h1>
+<div id="content" class="whitebox">
+	<div class="padding4">
+		<div id="content-head" class="flex-container flex-align-space-around flex-align-top flex-nowrap">
+			<div><img src="images/logotype.jpg"></div>
+			<div class="flex-item desc">
+				<h1>Sample Contact</h1>
+				<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+			</div>
+		</div>
+		<br><hr><br>
+		<h4 class="regular"><a href="mailto:email@dominio.com.br"><i class="fa fa-envelope"></i> email@dominio.com.br</a></h4>
+		<br><br>
+		<div class="flex-container flex-align-left flex-align-middle flex-nowrap">
+			<div class="icon"><i class="fa fa-whatsapp"></i></div>
+			<div class="regular">
+				<h5><a href="tel:5500000000000">+55 00 00000-0000</a></h5>
+				<h5><a href="tel:5500000000000">+55 00 00000-0000</a></h5>
+			</div>
+		</div>
+		<br><br>
+		<div class="flex-container flex-align-left flex-align-middle flex-nowrap">
+			<div class="icon"><i class="fa fa-map-marker"></i></div>
+			<div class="regular">
+				<h5 class="regular">Pont des Arts,<br>75006 Paris, França</h5>
+			</div>
+		</div>
+	</div>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5250.045252938802!2d2.334634114599909!3d48.857778926542835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2zUGFyaXMsIEZyYW7Dp2E!5e0!3m2!1spt-BR!2sbr!4v1503417262791" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 <?php }
 }
--- templates/compiled/bfbdc639ea33737932bcdf475d8da8ba5189da65_0.file.index.html.php
+++ templates/compiled/bfbdc639ea33737932bcdf475d8da8ba5189da65_0.file.index.html.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2017-08-22 05:26:44
+/* Smarty version 3.1.29, created on 2017-08-22 13:24:15
   from "C:\wamp64\www\capture-form\templates\index.html" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_599beac44c8ce6_21089480',
+  'unifunc' => 'content_599c5aafb07532_21988611',
   'file_dependency' => 
   array (
     'bfbdc639ea33737932bcdf475d8da8ba5189da65' => 
     array (
       0 => 'C:\\wamp64\\www\\capture-form\\templates\\index.html',
-      1 => 1503390393,
+      1 => 1503419054,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:form.html' => 1,
   ),
 ),false)) {
-function content_599beac44c8ce6_21089480 ($_smarty_tpl) {
+function content_599c5aafb07532_21988611 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="ptbr">
@@ -40,6 +40,7 @@ function content_599beac44c8ce6_21089480 ($_smarty_tpl) {
 		<link rel="stylesheet" href="css/global.css" type="text/css" media="screen" charset="utf-8"/>
 		<link rel="stylesheet" href="css/estilo.css" type="text/css" media="screen" charset="utf-8"/>
 		<link rel="stylesheet" href="css/theme.css" type="text/css" media="screen" charset="utf-8"/>
+		<link rel="stylesheet" href="css/content.css" type="text/css" media="screen" charset="utf-8"/>
 		<link rel="stylesheet" href="css/font-awesome.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
 		<link rel="stylesheet" href="css/font-worksans.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
 		<link rel="stylesheet" href="css/font-pathway.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
@@ -56,7 +57,7 @@ function content_599beac44c8ce6_21089480 ($_smarty_tpl) {
 	<body>
 		<?php echo (($tmp = @$_smarty_tpl->tpl_vars['printDebug']->value)===null||$tmp==='' ? '' : $tmp);?>
 
-		<div class="master flex-container flex-align-center flex-align-middle">
+		<div class="master flex-container flex-align-center flex-align-top">
 			<?php $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:content.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
